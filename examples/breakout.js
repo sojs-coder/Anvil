2e6a37f7ef336ce38f4ef838de6e946b86d24f78
SoJS coder <matfuller703@gmail.com>
2024-02-19 11:15:18 -0800

    Update game examples with line count and comments

--- examples/breakout.js
+++ examples/breakout.js
@@ -1,11 +1,15 @@
-
+// import everything from ANVIL
 const {
     Scene, SceneManager, Polygon, Input
 } = ANVIL;
+
+// create a new scene with a black background and an update function
 const scene = new Scene({
     backgroundColor: 'black',
     update
 });
+
+// create a paddle and a ball
 const paddle = new Polygon({
     points: [
         [0, 0],
@@ -15,6 +19,7 @@ const paddle = new Polygon({
     ],
     backgroundColor: 'white',
 });
+// the paddle starts at the bottom of the screen
 paddle.move([350, 450]);
 scene.addObject(paddle);
 const ball = new Polygon({
@@ -26,9 +31,13 @@ const ball = new Polygon({
     ],
     backgroundColor: 'white',
 });
+// the ball starts in the middle of the screen
 ball.move([350, 250]);
+// here we use the meta property to store the velocity of the ball
 ball.meta.velocity = [1, 2];
 scene.addObject(ball);
+
+// move the paddle left and right with the arrow keys
 const left = new Input("ArrowLeft", 10);
 const right = new Input("ArrowRight", 10);
 left.on = () => {
@@ -39,7 +48,10 @@ right.on = () => {
 }
 left.activate();
 right.activate();
+
+// here we enable collisions between the paddle and the ball
 scene.enableCollisionsBetween(paddle, ball, () => {
+    // when the ball hits the paddle, we reverse its y velocity, and assign a new x velocity based on the position of the ball relative to the paddle
     var ballCenter = ANVIL.getCentroid(ball.polify());
     var paddleCenter = ANVIL.getCentroid(paddle.polify());
     var ballX = ballCenter[0];
@@ -50,6 +62,9 @@ scene.enableCollisionsBetween(paddle, ball, () => {
     ball.meta.velocity[1] = -1 * ball.meta.velocity[1];
     ball.meta.velocity[0] = diff / 10;
 }, () => { });
+
+// generate the blocks. We use a nested for loop to create 8 rows of 6 blocks each
+// the points are [1,1], [99,1], [99,19], [1,19] to create a 100x20 block with a gap of 1px between each block
 function genBlock(i,j){
     var block = new Polygon({
         points: [
@@ -63,6 +78,8 @@ function genBlock(i,j){
     });
     scene.addObject(block);
     block.move([i * 100, j * 20]);
+
+    // when the ball collides with a block, remove the block and make the ball bounce
     scene.enableCollisionsBetween(ball, block, () => {
         ball.meta.velocity[1] = -1 * ball.meta.velocity[1];
         blocks = blocks.filter(b => b.id !== block.id);
@@ -78,6 +95,8 @@ for (var i = 0; i < 8; i++) {
         genBlock(i,j);
     }
 }
+
+// all we need to update is the ball position and check to see if it is past the walls
 function update() {
     ball.move(ball.meta.velocity)
     var right = ball.points[1][0];
@@ -93,11 +112,14 @@ function update() {
     if (bottom >= 500) {
         ball.move([0, -250]);
     }
+    // and also check for a win
     if(blocks.length === 0){
         alert('You win!');
         window.location.reload();
     }
 }
+
+// intialize the game!
 const manager = new SceneManager({
     initialScene: scene,
     canvas: document.getElementById('can'),
