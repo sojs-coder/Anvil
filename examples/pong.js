23e3ceecc20a498209a91b7f87d95768821e4ed0
SoJS coder <matfuller703@gmail.com>
2024-02-25 10:53:16 -0800

    Add Pong game card and commented out pong.js

--- examples/pong.js
+++ examples/pong.js
@@ -1,10 +1,13 @@
+// import everything from the ANVIL library
 const { Scene, SceneManager, Polygon, Input, Text } = ANVIL;
 
+// create a new scene with a black background and an update function
 const scene = new Scene({
     backgroundColor: 'black',
     update
 });
 
+// create the paddle polygons
 const p1Paddle = new Polygon({
     points: [
         [0, 0],
@@ -14,7 +17,6 @@ const p1Paddle = new Polygon({
     ],
     backgroundColor: 'white',
 });
-
 const p2Paddle = new Polygon({
     points: [
         [0, 0],
@@ -25,14 +27,15 @@ const p2Paddle = new Polygon({
     backgroundColor: 'white',
 });
 
+// move the paddles to their starting positions and set their bounds
 p1Paddle.move([20, 200]);
 p2Paddle.move([460, 200]);
-
 p1Paddle.setBounds([500, 500]);
 p2Paddle.setBounds([500, 500]);
 scene.addObject(p1Paddle);
 scene.addObject(p2Paddle);
 
+// create the ball polygon
 const ball = new Polygon({
     points: [
         [0, 0],
@@ -42,16 +45,18 @@ const ball = new Polygon({
     ],
     backgroundColor: 'white',
 });
+// move the ball to its starting position and set its velocity
 ball.move([240, 240]);
 ball.meta.velocity = [1, 0];
 scene.addObject(ball);
 
+// add input monitors for each paddle
 const p1Up = new Input("w", 10);
 const p1Down = new Input("s", 10);
 const p2Up = new Input("ArrowUp", 10);
 const p2Down = new Input("ArrowDown", 10);
 
-
+// add walls so that we can easily detect when the ball goes out of bounds
 const rWall = new Polygon({
     points: [
         [0, 0],
@@ -80,10 +85,14 @@ scene.enableCollisionsBetween(ball, lWall, () => {
     p2Score.text = (parseInt(p2Score.text) + 1).toString();
     resetBall(-1);
 }, () => { });
+
+// reset the ball on collision with the walls
 function resetBall(direction) {
     ball.moveTo([240, 240])
     ball.meta.velocity = [direction, 0];
 }
+
+// create the net in the middle of the screen
 for (var i = 0; i < 500; i += 20) {
     const net = new Polygon({
         points: [
@@ -96,6 +105,8 @@ for (var i = 0; i < 500; i += 20) {
     });
     scene.addObject(net);
 }
+
+// create the score text
 const p1Score = new Text({
     text: "0",
     fontSize: 30,
@@ -111,6 +122,7 @@ const p2Score = new Text({
 scene.addObject(p1Score);
 scene.addObject(p2Score);
 
+// add the input monitors and collision monitors to the paddles
 p1Up.on = () => {
     p1Paddle.move([0, -5]);
 }
@@ -127,11 +139,9 @@ p1Up.activate();
 p1Down.activate();
 p2Up.activate();
 p2Down.activate();
-
 scene.enableCollisionsBetween(p1Paddle, ball, () => {
     ballPaddleCollision(ball, p1Paddle);
 }, () => { });
-
 scene.enableCollisionsBetween(p2Paddle, ball, () => {
     ballPaddleCollision(ball, p2Paddle);
 }, () => { });
@@ -144,6 +154,8 @@ function ballPaddleCollision(ball, paddle) {
     ball.meta.velocity[1] = diff / 10;
     ball.meta.velocity = ANVIL.multArrays(ball.meta.velocity, [-1.1, 1.1]);
 }
+
+// move the ball every tick
 function update() {
     ball.move(ball.meta.velocity);
     var ballBottom = ball.points[2][1];
@@ -153,6 +165,7 @@ function update() {
     }
 }
 
+// initialize everything!
 const manager = new SceneManager({
     initialScene: scene,
     width: 500,
